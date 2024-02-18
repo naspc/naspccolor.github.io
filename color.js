31477b01a8dae0d3201a8dca19930cb4eb9dfb7a
naspc <nassirpc@gmail.com>
2024-02-18 14:55:30 -0800

    function 1

--- color.js
+++ color.js
@@ -2,7 +2,9 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f1f5f8"];
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
-btn.addEventListener('click', function()) {
+btn.addEventListener('click', function() {
 // get random number between 0 and 3
 const randomNumber = 2;
-}
+document.body.style.backgroundColor = colors[randomNumber];
+color.textContent = colors[randomNumber];
+});
